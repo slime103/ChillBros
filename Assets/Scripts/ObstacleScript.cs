5739aa8e434081d5012929b53d96077b5321089b
RePleX49 <lukelin2009@hotmail.com>
2019-04-28 18:13:08 -0400

    Added Obstacle Spawner
    - Added obstacle Spawner
    - Added base ObstacleScript

---
+++ Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = rb.position + Vector2.down * moveSpeed * Time.deltaTime;
+        rb.MovePosition(newPos);
+    }
+}
--- Assets/Scripts/ObstacleSpawnerScript.cs
+++ Assets/Scripts/ObstacleSpawnerScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class ObstacleSpawnerScript : MonoBehaviour
 {
+    private Vector2 spawnLocation;
+    [SerializeField] GameObject[] Obstacles;
+
+    [SerializeField] float minSpawnX;
+    [SerializeField] float maxSpawnX;
+    [SerializeField] float SpawnInterval;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnLocation = this.gameObject.transform.position;
+        InvokeRepeating("SpawnObstacle", 0.2f, SpawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle()
+    {
+        if(Obstacles.Length > 0)
+        {
+            spawnLocation.x = Random.Range(minSpawnX, maxSpawnX);
+
+            GameObject a = Instantiate(Obstacles[Random.Range(0, Obstacles.Length)]);
+
+            a.transform.position = spawnLocation;
+        }       
     }
 }
