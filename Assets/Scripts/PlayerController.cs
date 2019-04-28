cb9be124f6297e7c192ce01a4aa18fb9243078d5
RePleX49 <lukelin2009@hotmail.com>
2019-04-28 14:13:04 -0400

    Added Working Player Controls
    - Move side to side with A and D working
    - Ragdoll boi attached by Distance Joint, swinging working decently well

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Vector2 newPos;
+    private int moveDirection;
+    private float moveSpeed = 10.0f;
+    private float InitialYPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        InitialYPos = rb.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetMoveDirection();
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            //Move player position respectively when left or right input is true
+            newPos = new Vector2(rb.position.x + (moveDirection * moveSpeed * Time.deltaTime), InitialYPos);
+        }
+
+        rb.MovePosition(newPos);
+    }
+
+    void GetMoveDirection()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            moveDirection = 1;
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            moveDirection = -1;
+        }
+        else
+        {
+            moveDirection = 0;
+        }
     }
 }
