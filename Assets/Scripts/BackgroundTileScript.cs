9c81207b434babaa9a986d817bc1a6c24e3c51f6
Luke Lin <lukelin2009@hotmail.com>
2019-05-01 12:40:49 -0400

    Semi Working Background Scrolling

---
+++ Assets/Scripts/BackgroundTileScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTileScript : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = this.transform.position + Vector3.down * moveSpeed * Time.deltaTime;
+        this.transform.position = newPos;
+    }
+}
---
+++ Assets/Scripts/BackgroundTilerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTilerScript : MonoBehaviour
+{
+    public GameObject BackgroundTile;
+    public float SpawnInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnTiles", 0.0f, SpawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnTiles()
+    {
+        GameObject a = null;
+
+        a = Instantiate(BackgroundTile);
+        a.transform.position = this.transform.position;
+    }
+}
