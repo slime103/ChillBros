693de8d33ca149f505e32d352cbc3e40e7d5117f
Slime103 <slime103@users.noreply.github.com>
2019-05-08 12:45:18 -0400

    RadioScript
    Big BIG... hotdog

---
+++ Assets/Scripts/RadioScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioScript : MonoBehaviour
+{
+    public int volume;
+    public AudioSource[] playlist;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextTrack()
+    {
+
+    }
+
+    public void LastTrack()
+    {
+
+    }
+
+    //RadioOFF
+    //RadioON
+}
