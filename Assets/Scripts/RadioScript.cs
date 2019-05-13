b155d4df3ce657ee27f20fadfe6063d5db3aefbc
Slime103 <slime103@users.noreply.github.com>
2019-05-13 11:11:57 -0400

    Radio
    bing bing

--- Assets/Scripts/RadioScript.cs
+++ Assets/Scripts/RadioScript.cs
@@ -4,29 +4,52 @@ using UnityEngine;
 
 public class RadioScript : MonoBehaviour
 {
-    public int volume;
-    public AudioSource[] playlist;
+    public float volume;
+    public AudioClip[] playlist;
+    AudioSource radio;
+    public int currentSong = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        radio = GetComponent<AudioSource>();
+        int num = Random.Range(0, 7);
+        radio.PlayOneShot(playlist[num]);
+        currentSong = num;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.S))
+        {
+            radio.Stop();
+            int num = Random.Range(0, 7);
+            radio.PlayOneShot(playlist[num]);
+            currentSong = num;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            NextTrack();
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            LastTrack();
+        }
     }
 
     public void NextTrack()
     {
-
+        radio.Stop();
+        currentSong++;
+        radio.PlayOneShot(playlist[currentSong]);
     }
 
     public void LastTrack()
     {
-
+        radio.Stop();
+        currentSong--;
+        radio.PlayOneShot(playlist[currentSong]);
     }
 
     //RadioOFF
